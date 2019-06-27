33d3f8498e7c3e0689540d7aca6c5d3f584ab1f9
Kostlba <mrjyraa@gmail.com>
2019-06-28 01:28:27 +0300

    Update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class OrdersController < ApplicationController
-  def index; end
+  def index
+    @message = 'Input ID and Fullname'
+  end
 
   def find
     @fullname = params[:fullname]
@@ -10,11 +12,9 @@ class OrdersController < ApplicationController
 
       redirect_to order_path(@order.id, @fullname)
     else
-      @massage = ''
-      @massage += 'Ви не ввели ключ ' if params[:id] == '' || params[:id].nil?
-      @massage += 'Ви не ввели fullname ' if @fullname == '' || @fullname.nil?
-      @massage += 'НЕ ЗНАЙДЕНО TOB З ТАКИМ ключом ' unless @order && params[:id] != ''
-      @massage += 'Fullname не співпадає ' if @order && @order.fullname != @fullname && @fullname != ''
+      @message = 'Ви не ввели fullname ' if @fullname == '' || @fullname.nil?
+      @message = 'Not found' unless @order && params[:id] != ''
+      @message = 'Fullname не співпадає ' if @order && @order.fullname != @fullname && @fullname != ''
       render 'orders/index'
     end
   end
